54fa4980c27d75da8fd218eae2196d378f0f93e9
Denis Semenov <hello@denisemenov.com>
2025-01-29 21:54:25 +0100

    Add dependencies Update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-  ],
+  modules: ['@nuxt/content', '@nuxt/fonts', '@nuxt/image', '@nuxtjs/i18n'],
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
   compatibilityDate: '2024-04-03',
-})
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ['~/assets/css/main.css'],
+});
